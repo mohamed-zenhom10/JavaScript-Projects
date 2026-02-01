fa25764e15e06bb5188f832ca3b2cdce5196d806
mohamed-zenhom10 <mzenhom763@gmail.com>
2026-02-01 14:26:26 +0200

    Update Travel Website , Adding Scroll Animation

--- Travel Website/js/main.js
+++ Travel Website/js/main.js
@@ -17,3 +17,4 @@ window.onscroll = () => {
 }
 
 // End Header Scripting
+
---
+++ Travel Website/js/scroll.js
@@ -0,0 +1,66 @@
+const sr = ScrollReveal();
+
+sr.reveal(".home-content" , {
+  origin : "left",
+  distance: "20px",
+  duration: 1000,
+  delay: 500,
+  easing: "ease",
+})
+
+sr.reveal(".home-img" , {
+  origin : "right",
+  distance: "20px",
+  duration: 1000,
+  delay: 500,
+  easing: "ease",
+})
+
+sr.reveal(".img-card" , {
+  origin: "bottom",
+  distance: "20px",
+  duration: 1000,
+  easing: "ease",
+  interval: 200,
+})
+
+sr.reveal(".image" , {
+  origin: "bottom",
+  distance: "20px",
+  duration: 1000,
+  delay: 500,
+  easing: "ease",
+})
+
+
+sr.reveal(".content" , {
+  origin: "bottom",
+  distance: "20px",
+  duration: 1000,
+  delay: 500,
+  easing: "ease",
+})
+
+sr.reveal(".card" , {
+  origin: "bottom" , 
+  distance: "20px",
+  duration: 1000,
+  easing: "ease",
+  interval: 150,
+})
+
+sr.reveal(".img" , {
+  origin : "left" ,
+  distance: "20px",
+  duration: 1000,
+  delay: 150,
+  easing: "ease"
+})
+
+sr.reveal(".about-content" , {
+  origin : "right" ,
+  distance: "20px",
+  duration: 1000,
+  delay: 150,
+  easing: "ease"
+})
